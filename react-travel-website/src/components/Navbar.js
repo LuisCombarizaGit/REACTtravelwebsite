d5223e9fba6c0d4409c7958cde4263fef9bec5e2
Luis Combariza <luis_combariza@outlook.com>
2021-02-23 14:16:55 -0700

    updated Navbar.js to include sections of website

--- react-travel-website/src/components/Navbar.js
+++ react-travel-website/src/components/Navbar.js
@@ -1,15 +1,54 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { BroweserRouter as Router, Switch, Route } from "react-router-dom";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            PROJECT--TRAVEL <i className="fas fa-globe-americas" />
+            Xplore <i className="fas fa-globe-americas" />
           </Link>
+          <div className="manu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}></ul>
+          <li ClassName="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li ClassName="nav-item">
+            <Link
+              to="/services"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Services
+            </Link>
+          </li>
+          <li ClassName="nav-item">
+            <Link
+              to="/products"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Products
+            </Link>
+          </li>
+          <li ClassName="nav-item">
+            <Link
+              to="/sign-up"
+              className="nav-links-mobile"
+              onClick={closeMobileMenu}
+            >
+              Sign Up
+            </Link>
+          </li>
         </div>
       </nav>
     </>
