3f8dfee4a373faaf7f36d0a51d9be5c912047ace
Luis Combariza <luis_combariza@outlook.com>
2021-02-23 15:29:52 -0700

    Updated HoreSection.js

--- react-travel-website/src/components/HeroSection.js
+++ react-travel-website/src/components/HeroSection.js
@@ -1,5 +1,7 @@
 import React from "react";
+import "../App.css";
 import { Button } from "./Button";
+import "./HeroSection.css";
 
 function HeroSection() {
   return (
@@ -14,6 +16,13 @@ function HeroSection() {
           buttonSize="btn--large"
         >
           GET STARTED
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          WATCH TRAILER <i className="far fa-play-circle"></i>
         </Button>
       </div>
     </div>
