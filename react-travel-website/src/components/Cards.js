a57758904af3a690dfba9862cbac29c1d425eb28
Luis Combariza <luis_combariza@outlook.com>
2021-02-24 10:29:47 -0700

    updated Card.js file

--- react-travel-website/src/components/Cards.js
+++ react-travel-website/src/components/Cards.js
@@ -9,7 +9,38 @@ function Cards() {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem />
+            <CardItem
+              src="\images\img-9.jpg"
+              text="Xplore wonderfull waterfalls in the rain forest"
+              label="Adventure"
+              pat="/services"
+            />
+            <CardItem
+              src="\images\img-2.jpg"
+              text="Xplore never ending beaches in the Caribbean"
+              label="Luxury"
+              pat="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="\images\img-1.jpg"
+              text="Xplore Canadian outback and Rockies"
+              label="Camping"
+              pat="/services"
+            />
+            <CardItem
+              src="\images\img-5.jpg"
+              text="Xplore New York city's busy night life"
+              label="City"
+              pat="/services"
+            />
+            <CardItem
+              src="\images\img-8.jpg"
+              text="Xplore cammel ride Egyptian dunes "
+              label="Desert"
+              pat="/services"
+            />
           </ul>
         </div>
       </div>
